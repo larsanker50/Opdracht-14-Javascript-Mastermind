7642409f2b3c85a4b9d6afdc9527b6f8a3ebc729
larsanker50 <75078152+larsanker50@users.noreply.github.com>
2021-02-08 15:51:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,26 @@
-// CR :: try using const and let properly
-let playerArray = [];
-let gameOverArray = [];
+let AantalGoed = 0;
+let endGame = false;
+let beurt = 0;
+const playerArray = [];
+const gameOverArray = [];
+const myTableDiv = document.getElementById("javascriptTable");
+const table = document.createElement("table");
+const tableBody = document.createElement("tbody");
 
-// CR :: Try to better describe your variables
-let x = 0;
-let m = false;
 
-// CR :: You can make this shorter, try to refactor this code - ask for help if needed
-function addTable() {
-  let myTableDiv = document.getElementById("javascriptTable");
-
-  let table = document.createElement("table");
-
-  let tableBody = document.createElement("tbody");
-  table.appendChild(tableBody);
-
-  i = 0;
-
-  document.getElementById("button").onclick = function () {
-    controlFunction(i);
-
-    if (m) return console.log();
-    if (i < 12) {
+//deze functie maakt een nieuwe beurt aan.
+function addBeurt() {
+  if (endGame) return console.log("Game is Over");
+    if (beurt < 12) {
       let tr = document.createElement("tr");
       tableBody.appendChild(tr);
-      for (let j = 0; j < 5; j++) {
+      for (let tableLength = 0; tableLength < 5; tableLength++) {
         let td = document.createElement("td");
         tr.appendChild(td);
-        if (j > 0) {
+        if (tableLength > 0) {
           let sel = td.appendChild(document.createElement("select"));
-          sel.classList.add("row" + i);
-          sel.classList.add("collumn" + j);
+          sel.classList.add("row" + beurt);
+          sel.classList.add("collumn" + tableLength);
           let opt1 = td.appendChild(document.createElement("option"));
           let opt2 = td.appendChild(document.createElement("option"));
           let opt3 = td.appendChild(document.createElement("option"));
@@ -58,74 +48,68 @@ function addTable() {
           sel.add(opt4);
           sel.add(opt5);
         }
-        if (j == 0) {
-          let number = i + 1;
+        if (tableLength == 0) {
+          let number = beurt + 1;
           td.appendChild(document.createTextNode(number));
         }
       }
-      console.log(m);
-      if (i <= 11) {
-        i++;
+      console.log(endGame);
+      if (beurt <= 11) {
+        beurt++;
       }
     }
     myTableDiv.appendChild(table);
-    //alert("Dit is Ronde " + (i + 1));
-  }; // einde van de table create
-  //dit is de controlfunctie. ik weet dat het niet mooi is maar het werkt zo. ik had liever deze functie op lijn 18 uitgeschreven.
-  function controlFunction(i) {
-    const testArray = [];
+};
 
-    if (i > 0) {
-      for (let t = 0; t < 4; t++) {
-        el = parseInt(document.getElementsByClassName("row" + (i - 1))[t].value);
-        testArray.push(el);
-      }
-    }
-    playerArray = testArray;
-    // dit is het einde van playerarray
-    controlArray = [];
-    control = {
-      1: 0, //hoeveel white (dus juiste kleur maar niet op juiste plaats)
-      2: 0, // hoeveel red (dus juiste kleur op de juiste plek)
-    };
-    x = 0;
-    for (h = 0; h < 4; h++) {
-      if (
-        colorArray[h] == playerArray[h] &&
-        colorArray.includes(playerArray[h])
-      ) {
-        controlArray.push("red");
-        control[2] = control[2] + 1;
-        x++;
-      } else if (playerArray.includes(colorArray[h])) {
-        controlArray.push("white");
-        control[1] = control[1] + 1;
-      }
-    }
-    if (x >= 4) {
-      alert(
-        "Gefeliciteerd!!! je hebt gewonnen!! De juiste kleurencombinatie was " +
-        gameOverArray
-      );
-      m = true;
-      return;
-    }
-    if (i > 0) {
-      document.getElementById("one").innerHTML = control[1];
-      document.getElementById("two").innerHTML = control[2];
-      document.getElementById("controleHeader").innerHTML =
-        "Controletabel beurt " + i;
-    }
-    if (i == 12 && colorArray !== playerArray) {
-      alert(
-        "Game Over. De juiste kleurencombinatie was " +
-        gameOverArray +
-        ". Probeer het eens opnieuw :)"
-      );
-    }
-  } //einde controlefunctie
-}
 
+//controlfunctie. controleert of de array goed is.
+function controlFunction(beurt) {
+  if (beurt > 0) {
+    playerArray.splice(0, playerArray.length);
+    for (let classNumber = 0; classNumber < 4; classNumber++) {
+      el = parseInt(document.getElementsByClassName("row" + (beurt - 1))[classNumber].value);
+      playerArray.push(el);
+    }
+  }
+  controlArray = [];
+  control = {
+    1: 0, //hoeveel white (dus juiste kleur maar niet op juiste plaats)
+    2: 0, // hoeveel red (dus juiste kleur op de juiste plek)
+  };
+  AantalGoed = 0;
+  for (ArrayLength = 0; ArrayLength < 4; ArrayLength++) {
+    if (
+      colorArray[ArrayLength] == playerArray[ArrayLength] &&
+      colorArray.includes(playerArray[ArrayLength])
+    ) {
+      controlArray.push("red");
+      control[2] = control[2] + 1;
+      AantalGoed++;
+    } else if (playerArray.includes(colorArray[ArrayLength])) {
+      controlArray.push("white");
+      control[1] = control[1] + 1;
+    }};
+  if (beurt > 0) {
+    document.getElementById("one").innerHTML = control[1];
+    document.getElementById("two").innerHTML = control[2];
+    document.getElementById("controleHeader").innerHTML = "Controletabel beurt " + beurt;
+  };
+  if (AantalGoed >= 4) {
+    alert(
+      "Gefeliciteerd!!! je hebt gewonnen!! De juiste kleurencombinatie was " +
+      gameOverArray
+    );
+    endGame = true;
+    return;
+  };
+  if (beurt == 12 && colorArray !== playerArray) {
+    alert(
+      "Game Over. De juiste kleurencombinatie was " +
+      gameOverArray +
+      ". Probeer het eens opnieuw :)"
+  )}};
+
+//deze functie maakt een willekeurige combinatie van 4 kleuren aan
 function addCombination() {
   colorArray = [];
   colors = {
@@ -155,11 +139,19 @@ function addCombination() {
       gameOverArray.push("bruin");
     }
   }
-}
+};
 
+//dit is de eventlistner voor de knop en roept 2 functies aan.
+document.getElementById("button").onclick = function () {
+  controlFunction(beurt);
+  addBeurt();
+};
+
+//hier start het script
 addCombination();
-addTable();
+table.appendChild(tableBody);
 console.log(colorArray);
+
 
 /* 1 = blue
 2 = green
